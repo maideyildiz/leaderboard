95d5656d504e8f2dd320b4b5d05cfe7a62de6c3c
Maide Yıldız <maideyildizz@gmail.com>
2024-12-28 01:21:40 +0300

    fixes

--- src/controller/leaderboard.controller.js
+++ src/controller/leaderboard.controller.js
@@ -1,49 +1,38 @@
 import player from '../models/player.model.js';
-import {redisClient} from '../database/redis.database.js';
-import { getPlayerName } from '../helpers/player.helper.js';
+import { getRank,setPlayerZScore,getPlayerZScore,getFromCache  } from '../helpers/redis.helper.js';
+import {getUserByIdFromDb} from '../services/user.service.js';
+import { getPlayerByUserIdFromDb,addPlayerToDb,updatePlayerInDb,getPlayerRankFromDb } from '../services/player.service.js';
 const submitScore = async (req, res, next) => {
     try {
         const { score, gameId } = req.body
-        let userId=req.body.userId
-        if(!userId){
-          userId=req.baseUserId
+        let userId=req.baseUserId
+
+        if (typeof score !== 'number' || !Number.isInteger(score)) {
+            return res.status(400).json({ message: 'Score should be an integer' });
         }
-        let updateScore = true
-        const cachedScore = await redisClient.zScore('leaderboard', userId)
-        if (cachedScore) {
-          if (score > cachedScore) {
-            const result = await player.updateOne({ userId }, { score })
-            if (result.modifiedCount === 1) {
-              const cachedPlayer = await redisClient.get(`player:${userId}`)
-              if(cachedPlayer){
-                await redisClient.set(`player:${userId}`, JSON.stringify({ userId, userName: cachedPlayer.userName, score, gameId }))
-              }
+        if (typeof gameId !== 'string') {
+            return res.status(400).json({ message: 'GameId should be a string' });
+        }
+
+          let existingPlayer = getFromCache("player",userId)
+          existingPlayer = !existingPlayer ? await getPlayerByUserIdFromDb(userId ) : existingPlayer
+          if(!existingPlayer){
+            let playerUser = await getFromCache("user",userId)
+            playerUser= !playerUser ? await getUserByIdFromDb(userId) : playerUser
+            if(!playerUser){
+              res.status(404).json({ message: 'User not found' });
             }
-          }else{
-            updateScore = false
+            var addedPlayer = await addPlayerToDb({ gameId, userId, score})
+            await addPlayerToUser(playerUser._id,addedPlayer._id)
           }
-        } else {
-          const existingPlayer = await player.findOne({ userId })
-          if (existingPlayer && score > existingPlayer.score) {
-              await player.updateOne({ userId, gameId }, { score })
-              await redisClient.set(`player:${userId}`, JSON.stringify({ userId, userName: existingPlayer.userName, score, gameId }))
-          } else {
-            const playerCountFromRedis = await redisClient.get('player:count')
-            const playerCount = playerCountFromRedis ? parseInt(playerCountFromRedis) : await player.countDocuments({})
-            const userName = `player${playerCount + 1}`
-
-            await player.create({ userId, userName, score, gameId })
-            await redisClient.set(`player:${userId}`, JSON.stringify({ userId, userName, score, gameId }));
+          else if (existingPlayer && score > existingPlayer.score) {
+              existingPlayer.score = score
+              await updatePlayerInDb(existingPlayer._id, existingPlayer)
           }
-        }
-        if(updateScore){
-        await redisClient.zAdd('leaderboard', {
-          score,
-          value: userId,
-        })}
 
-        const rank = await redisClient.zRevRank('leaderboard', userId) + 1
-        res.status(200).json({userId,gameId, score,rank: rank})
+        setPlayerZScore(score, userId)
+        const rank = getRank(userId)
+        res.status(200).json({userId, gameId, score, rank: rank})
 
     } catch (error) {
         next(error);
@@ -95,7 +84,9 @@ const getTopPlayers = async (req, res, next) => {
 
         const leaderboardWithRank = await Promise.all(
           leaderboard.map(async (entry, index) => {
-            const userName = await getPlayerName(entry.value);
+            let user = await getFromCache("user",entry.value);
+            user = !user ? await getUserByIdFromDb(entry.value) : user
+            let userName = !user ? `Unknown User` : user.userName
             return {
               rank: start + index + 1,
               username: userName,
@@ -118,28 +109,28 @@ const getTopPlayers = async (req, res, next) => {
 
 const getPlayerRank = async (req, res, next) => {
     try {
-        const userId = req.query?.userId;
+        let userId = req.query?.userId;
         if (!userId) {
-          return res.status(400).json({ message: 'User ID is required' });
+          userId=req.baseUserId
         }
 
         let rank, score;
 
-        const cachedScore = await redisClient.zScore('leaderboard', userId);
+        const cachedScore = await getPlayerZScore( userId);
         if (cachedScore !== null) {
-          rank = await redisClient.zRevRank('leaderboard', userId) + 1;
+          rank = await getRank(userId) + 1;
           score = cachedScore;
         } else {
-          const playerData = await player.findOne({ userId });
+          const playerData = await getPlayerByUserIdFromDb(userId);
           if (!playerData) {
             return res.status(404).json({ message: 'Player not found' });
           }
 
           score = playerData.score;
 
-          rank = (await player.countDocuments({ score: { $gt: playerData.score } })) + 1;
+          rank = await getPlayerRankFromDb(playerData.score);
 
-          await redisClient.zAdd('leaderboard', { score: playerData.score, value: userId });
+          await setPlayerZScore(playerData.scoreuserId);
         }
 
         res.status(200).json({ userId, rank, score });
--- src/helpers/redis.helper.js
+++ src/helpers/redis.helper.js
@@ -1,4 +1,23 @@
-const cachedPlayer = await redisClient.get(`player:${userId}`)
-if(cachedPlayer){
-  await redisClient.set(`player:${userId}`, JSON.stringify({ userId, userName: cachedPlayer.userName, score, gameId }))
+import {redisClient} from '../database/redis.database.js';
+
+const getFromCache = async (key,id) => {
+    const cachedUser = await redisClient.get(`${key}:${id}`);
+    return cachedUser ? JSON.parse(cachedUser) : null;
 }
+const setToCache = async (key,id,object) => {
+    await redisClient.set(`${key}:${id}`, JSON.stringify(object));
+}
+
+const getPlayerZScore = async (userId) => {
+    return await redisClient.zScore('leaderboard', userId);
+}
+const setPlayerZScore = async (score, userId) => {
+    await redisClient.zAdd('leaderboard', {score, value: userId});
+}
+
+const getRank = async (userId) => {
+    return await redisClient.zRevRank('leaderboard', userId) + 1;
+}
+
+
+export { getRank,setPlayerZScore,getPlayerZScore,setToCache,getFromCache };
